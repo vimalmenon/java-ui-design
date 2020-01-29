45ab386b1bbf89d93a6d99fdd3b7d04213df19e3
Vimal Menon <vimal.webs@gmail.com>
2020-01-29 08:19:51 +0800

    added pages controller and other minor chnages

---
+++ web/src/main/java/com/vimalmenon/application/pages/PagesController.java
@@ -0,0 +1,23 @@
+package com.vimalmenon.application.pages;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class PagesController {
+	
+	@RequestMapping(value = "")
+	public String index() {
+		return "index";
+
+	}
+	@RequestMapping(value = "**")
+	public String urlNotFound(HttpServletRequest request) 
+	{
+		return "index";
+	}
+
+}
