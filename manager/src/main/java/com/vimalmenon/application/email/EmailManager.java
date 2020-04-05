8ab6f0e8781671e99ea19cf685ed95449a4f7ca2
Vimal Menon <vimal.webs@gmail.com>
2020-04-05 19:43:06 +0800

    updated npm and other minor chnages

---
+++ manager/src/main/java/com/vimalmenon/application/email/EmailManager.java
@@ -0,0 +1,13 @@
+package com.vimalmenon.application.email;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailManager {
+
+	//@Autowired
+    //private JavaMailSender javaMailSender;
+	
+	
+	
+}
