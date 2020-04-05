4acdfb2bac76d09d07339e15efc87c4edbc00c6a
Vimal Menon <vimal.webs@gmail.com>
2020-04-05 22:11:52 +0800

    working on spring security

--- web/src/main/java/com/vimalmenon/application/security/ApplicationSecurity.java
+++ web/src/main/java/com/vimalmenon/application/security/ApplicationSecurity.java
@@ -12,11 +12,11 @@ public class ApplicationSecurity extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
-			.authorizeRequests()
-			.anyRequest()
-			.authenticated()
-			.and()
-			.httpBasic();
+	        .authorizeRequests()
+	        .anyRequest()
+	        .authenticated()
+	        .and()
+	        .httpBasic();
 	}
 
 	
