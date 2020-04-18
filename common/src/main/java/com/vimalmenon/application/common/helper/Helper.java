2c70c8717271d50fd076026ddd454ac57ce75633
Vimal Menon <vimalmenon@Vimals-MacBook-Pro.local>
2020-04-19 00:29:41 +0800

    removed jbcrypt refrence and othe minor in helper

--- common/src/main/java/com/vimalmenon/application/common/helper/Helper.java
+++ common/src/main/java/com/vimalmenon/application/common/helper/Helper.java
@@ -1,11 +1,9 @@
 package com.vimalmenon.application.common.helper;
 
-import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class Helper {
 	
-	private static final String salt = BCrypt.gensalt(12);
 	private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
 	public static String urlFixer(String url) {
