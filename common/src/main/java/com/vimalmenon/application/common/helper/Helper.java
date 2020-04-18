cf631819914a8c2c98c3641c45724b2ce8636d12
Vimal Menon <vimalmenon@Vimals-MacBook-Pro.local>
2020-04-19 00:24:22 +0800

    Working on helper encoder and matches

--- common/src/main/java/com/vimalmenon/application/common/helper/Helper.java
+++ common/src/main/java/com/vimalmenon/application/common/helper/Helper.java
@@ -1,25 +1,27 @@
 package com.vimalmenon.application.common.helper;
 
 import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class Helper {
 	
 	private static final String salt = BCrypt.gensalt(12);
-	
+	private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
-	public static String urlFixer (String url)
-	{
+	public static String urlFixer(String url) {
 		if (url.endsWith("/")) {
 			return url.substring(0, url.length() - 1);
 		}
 		return url;
 	}
-	public static boolean verifyPassword (String candidate, String passwordToVerify) 
-	{
-		return BCrypt.checkpw(candidate, passwordToVerify);
+
+	public static boolean verifyPassword(String candidate, String passwordToVerify) {
+		return encoder.matches(candidate, passwordToVerify);
+		//return BCrypt.checkpw(candidate, passwordToVerify);
 	}
-	public static String createPassword (String password) 
-	{
-		return BCrypt.hashpw(password, salt);
+
+	public static String createPassword(String password) {
+		return encoder.encode(password);
+		//return BCrypt.hashpw(password, salt);
 	}
 }
