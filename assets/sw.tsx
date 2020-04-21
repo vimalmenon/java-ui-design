213f4165655e6964fa122015c0eb9ac3a5d4c8dc
Vimal Menon <vimalmenon@Vimals-MacBook-Pro.local>
2020-04-21 19:48:56 +0800

    working on service worker

--- assets/sw.tsx
+++ assets/sw.tsx
@@ -1,3 +1,32 @@
 declare var VERSION:string;
+declare var caches:CacheStorage;
 
-console.log(VERSION);
+
+self.addEventListener("install", (event) => {
+	console.log(VERSION, "install is called");
+});
+
+self.addEventListener("activate", (event: any) => {
+	event.waitUntil(
+		caches.keys().then((cache) => {
+			return Promise.all(cache.filter((cacheName) => {
+				return cacheName !== `v${VERSION}`;
+			}).map((cacheName) => {
+				return caches.delete(cacheName);
+			}));
+		})
+	);
+});
+
+self.addEventListener("fetch", (event:  any) => {
+	caches.open(`v${VERSION}`).then((cache: any) => {
+		if ((event.request.url.endsWith("js") || event.request.url.endsWith("css")) && !event.request.url.startsWith("chrome-extension")) {
+			console.log(event.request);
+			fetch(event.request).then((response) => {
+				cache.put(event.request, response.clone());
+				return response;
+			});
+		}		
+	});
+	event.respondWith(fetch(event.request));
+});
