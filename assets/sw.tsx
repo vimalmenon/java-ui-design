cacbf5e3f4f18ea3687b27430ec6211df0823dd0
Vimal Menon <vimalmenon@Vimals-MacBook-Pro.local>
2020-04-21 20:12:04 +0800

    working on sw

--- assets/sw.tsx
+++ assets/sw.tsx
@@ -1,16 +1,21 @@
 declare var VERSION:string;
 declare var caches:CacheStorage;
+ var cacheList = ["/"];
+ var cacheVersion = `v${VERSION}`;
 
-
-self.addEventListener("install", (event) => {
-	console.log(VERSION, "install is called");
+self.addEventListener("install", (event: any) => {
+	event.waitUntil(
+		caches.open(cacheVersion).then((cache) => {
+			return cache.addAll(cacheList);
+		})
+	  );
 });
 
 self.addEventListener("activate", (event: any) => {
 	event.waitUntil(
 		caches.keys().then((cache) => {
 			return Promise.all(cache.filter((cacheName) => {
-				return cacheName !== `v${VERSION}`;
+				return cacheName !== cacheVersion;
 			}).map((cacheName) => {
 				return caches.delete(cacheName);
 			}));
@@ -19,9 +24,8 @@ self.addEventListener("activate", (event: any) => {
 });
 
 self.addEventListener("fetch", (event:  any) => {
-	caches.open(`v${VERSION}`).then((cache: any) => {
+	caches.open(cacheVersion).then((cache: any) => {
 		if ((event.request.url.endsWith("js") || event.request.url.endsWith("css")) && !event.request.url.startsWith("chrome-extension")) {
-			console.log(event.request);
 			fetch(event.request).then((response) => {
 				cache.put(event.request, response.clone());
 				return response;
