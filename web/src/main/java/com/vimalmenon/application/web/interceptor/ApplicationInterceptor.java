42786dff3ab889ad7575f4c569f0926c9012078e
Vimal Menon <vimalmenon@Vimals-MacBook-Pro.local>
2020-07-26 23:47:31 +0800

    Working on cors

--- web/src/main/java/com/vimalmenon/application/web/interceptor/ApplicationInterceptor.java
+++ web/src/main/java/com/vimalmenon/application/web/interceptor/ApplicationInterceptor.java
@@ -14,7 +14,7 @@ public class ApplicationInterceptor implements WebMvcConfigurer{
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedOrigins("http://localhost:8082").allowedMethods("*");
+		registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
 	}
 
 }
