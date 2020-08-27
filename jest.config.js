befe92811c1fffe6b60fa82090dd6325212fd603
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2020-08-27 22:51:05 +0800

    working on test

--- jest.config.js
+++ jest.config.js
@@ -8,10 +8,11 @@ module.exports = {
           tsConfig: 'tsconfig.json'
         }
     },
+    moduleDirectories: ["node_modules","assets"],
     moduleNameMapper: {
         "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":"<rootDir>/styleMock.js",
         "\\.(css|less)$": "<rootDir>/styleMock.js",
         "const" : "<rootDir>/assets/scripts/constants",
-      }
+    }
 };
   
