e3abcb98464d9022c50f0666a302b12d19df5dfe
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2020-09-05 12:57:33 +0800

    create tutorials topics and tutorials

---
+++ data/src/main/resources/db/migration/V19.00__create_table_tutorials.sql
@@ -0,0 +1,24 @@
+CREATE TABLE tutorials (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR (100) NOT NULL,
+    link VARCHAR (150) NOT NULL,
+    topic_id INT signed null,
+    youtube_url VARCHAR (250) NOT NULL,
+    notes VARCHAR (250) NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (topic_id) REFERENCES tutorial_topics (id),
+    CONSTRAINT UN_LINK UNIQUE (link),
+    CONSTRAINT UN_YOUTUBE_URL UNIQUE (youtube_url)
+);
+
+insert into tutorials(name,link,youtube_url)
+    values("Introduction to My Channel","introduction-to-my-channel", "https://www.youtube.com/watch?v=Wb1uty_bc3c");
+
+insert into tutorials(name,link,youtube_url)
+    values("Web Development Introduction","web-development-introduction", "https://www.youtube.com/watch?v=Jg-FAIH2yf4");
+
+insert into tutorials(name,link,youtube_url)
+    values("01 Introduction to HTML","introduction-to-html", "https://www.youtube.com/watch?v=kAHzqg7AVcs");
+
+insert into tutorials(name,link,youtube_url)
+    values("02 - More HTML tags and Attributes","more-html-tags-and-attributes", "https://www.youtube.com/watch?v=iyrdiFVaVtc&t=31s");
