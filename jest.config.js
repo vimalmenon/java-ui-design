d5a39fdf23cf112244530a1432634fd1beed4d74
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2020-09-27 15:04:55 +0800

    Package update and Make Test working

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,8 @@
 module.exports = {
-    verbose: true,
     preset: 'ts-jest',
-    transform: {
-        "\\.tsx$": ['babel-jest', 'ts-jest']
-    },
-    globals: {
-        'ts-jest': {
-          tsConfig: 'tsconfig.json'
-        }
-    },
-    moduleDirectories: ["node_modules","assets"],
-    moduleNameMapper: {
-        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":"<rootDir>/styleMock.js",
-        "\\.(css|less)$": "<rootDir>/styleMock.js",
-        "const" : "<rootDir>/assets/scripts/constants",
-    }
+    testEnvironment: 'node',
+    setupFilesAfterEnv: [
+      "<rootDir>/jest.setup.tsx"
+    ],
+    resolver: "jest-webpack-resolver"
 };
-  
